cadd8cbab067eddfeba72dc34f9b1d605fb754db
Rahul choudhary <rsc1401590@gmail.com>
2022-03-27 03:49:32 -0700

    intial commit

--- src/Preview.js
+++ src/Preview.js
@@ -1,5 +1,12 @@
 import { useState } from "react"
 import reviews from "./Data"
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 function Preview()
 {
     const len=reviews.length-1
@@ -7,24 +14,21 @@ function Preview()
   let {id,name,job,image,text}=reviews[index]
   function reset()
   {
-    document.getElementById('nextbtn').disabled=false
-    document.getElementById('btn2').disabled=false
+ 
       let a=0
       return setindex(index=0)
   }
   function next()
   {
-      console.log(index);
-      console.log(len);
-     if(index == len)
-     {
     
-    document.getElementById('nextbtn').disabled=true
+     if(index == len)
+      {
+       document.getElementById('nextBtn').style.visibility="hidden"
      return setindex(index)
      }
      else
      {
-        document.getElementById('btn2').disabled=false
+        document.getElementById('previious').style.visibility='visible'
     return  setindex(index+1)
      }
   }
@@ -33,31 +37,42 @@ function Preview()
     
      if(index === 0)
      {
-     document.getElementById('btn2').disabled=true
+     document.getElementById('previious').style.visibility='hidden'
      return setindex(index)
      }
      else
      {
-        document.getElementById('nextbtn').disabled=false
+        document.getElementById('nextBtn').style.visibility="visible"
     return  setindex(index-1)
      }
   }
     return(
-        <div>
-         {
-             <div>
-             <img src={image} width="200px" />
-             <h3> # id : {id}</h3>
-            <h1>{name}</h1>
-            <h2>{job}</h2>
-            <h5>{text}</h5>
-            <button id="btn2" onClick={()=>previous()}  >pre</button>
-            <button  id="nextbtn" onClick={()=>next()}  >next</button>
-            <button onClick={()=>reset()} >Reset</button>
-            </div>
-      }
-
-        </div>
-    )
+        <div style={{margin:"50px 0px 0px 380px" }} >
+         <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="170"
+        image={image}
+      />
+      <CardContent>
+    
+        <Typography align="inherit" gutterBottom variant="h6" component="div">
+        # {id}    {name}
+        </Typography>
+        <Typography variant="body1" color="text.secondary">
+          {text}
+        </Typography>
+      </CardContent>
+      <CardActions>
+          
+        <Button id="previious"  onClick={()=>previous()} size="small">Previous</Button>
+        <Button id="nextBtn" onClick={()=>next()} size="small">Next</Button>
+      <Button onClick={()=>reset()} >Reset</Button>
+      
+      </CardActions>
+    </Card>
+    </div>
+ );
 }
-export default Preview
+export default Preview;
